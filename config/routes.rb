8ebe0dac6704d7d36d624dd2655229fb141e6270
Zack Falgout <zfalgout@gmail.com>
2015-02-08 14:18:13 -0600

    Added navigation and welcome controller, set route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  #static pages don't have resources.
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+
+  #dynamic pages have resources.
   resources :invoices
 
-  root to: 'invoices#index'
+  root to: 'welcome#index'
+
+  
 end
