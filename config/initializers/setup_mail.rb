b6a1a7f51bfe07305d40f4c9d600b5c196477c5d
Zack Falgout <zfalgout@gmail.com>
2015-02-10 06:38:11 -0600

    changed sendgrid pw

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
+ ActionMailer::Base.delivery_method = :smtp
+ ActionMailer::Base.smtp_settings = {
 	:address			=>	'smtp.sendgrid.net',
 	:port				=> 	'587',
 	:authentication		=>	:plain,
 	:user_name			=>	'app33674646@heroku.com',
-	:password			=>	'email', 
+	:password			=>	'password', 
 	:domain				=> 	'heroku.com',
 	:enable_starttles_auto	=>	true
 }
